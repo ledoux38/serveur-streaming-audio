bed983329590aadc4be7991fea3394991754c85e
ledoux <ledoux.florian30@gmail.com>
2016-11-09 19:48:34 +0100

    rajout d'une version 2 de audiostream elle a pour but de faire des test pour regler le bug sonnore sans passé par la transmission de packet audio vers un client.

--- src/audiostream/audiostream.cpp
+++ src/audiostream/audiostream.cpp
@@ -1,6 +1,7 @@
 
 
 #include "audiostream.h"
+#if(VERSION_PROJET==1 || VERSION_PROJET ==0)
 audiostream::audiostream()
 {
 
@@ -67,3 +68,80 @@ void audiostream::initialization(unsigned int canaux, unsigned int taux_echantil
     initialize(canaux,taux_echantillonnage);
     m_initialiser = true;
 }
+#endif
+
+#if(VERSION_PROJET==2)
+audiostream::audiostream()
+{
+    m_decodeur=opus_decoder_create(FREQUENCE_SAMPLE_OPUS,2,&m_erreur);
+
+}
+
+
+void audiostream::load(std::array< sf::Int16,TABLE_SIZE> tableau)
+{
+
+
+    std::vector< sf::Int16>tmp;
+    tmp.reserve(TABLE_SIZE);
+    for(int i(0);i<TABLE_SIZE;i++)
+    {
+        tmp.push_back(tableau[i]);
+    }
+    m_echantillon.push(tmp);
+
+
+}
+
+    void audiostream::load(std::vector<opus_uint8> tableau)
+    {
+        int taille_decode;
+        std::vector<sf::Int16>echantillions(TAILLE_ABSOLU);
+        taille_decode=opus_decode(m_decodeur,&tableau[0],tableau.size(),&echantillions[0],VARIABLE_ELEMENTAIRE,0);
+        LOG("TAILLE_DECODE",taille_decode);
+        echantillions.resize(taille_decode);
+        m_echantillon.push(echantillions);
+
+    }
+
+bool audiostream::onGetData(Chunk &data)
+{        //const int echantillion_du_flux = TABLE_SIZE;
+
+
+         if(m_echantillon.size()>=RESERVE_SAMPLE)
+         {
+
+             m_echantillon.pop();
+             data.samples = &m_echantillon.front()[0];
+            //data.sampleCount = TABLE_SIZE;
+             LOG("sampleCount",m_echantillon.front().size());
+            data.sampleCount = m_echantillon.front().size();
+             return true;
+
+         }
+}
+
+
+void audiostream::onSeek(sf::Time timeoffset)
+{
+    //     m_echantillon_en_cours = static_cast<std::size_t>(timeoffset.asSeconds()+getSampleRate()*getChannelCount());
+}
+
+void audiostream::lecture_audio()
+{
+
+    play();
+    std::cout<<"lecture "<<std::endl;
+    while(this->getStatus() == audiostream::Playing)
+    {
+        sf::sleep(sf::seconds(0.1f));
+    }
+}
+
+void audiostream::initialization(unsigned int canaux, unsigned int taux_echantillonnage)
+{
+    initialize(canaux,taux_echantillonnage);
+    m_initialiser = true;
+}
+
+#endif
--- src/audiostream/audiostream.h
+++ src/audiostream/audiostream.h
@@ -31,6 +31,52 @@ private:
 
 };
 
+#if(VERSION_PROJET == 2)
+
+
+#ifndef SOUNDSTREAM_H
+#define SOUNDSTREAM_H
+//#include<SFML/Audio.hpp>
+//#include<SFML/Network.hpp>
+
+
+
+#include<iostream>
+#include<vector>
+#include<queue>
+
+#include"../define_serveur_client.h"
+
+
+class audiostream : public sf::SoundStream
+{
+public:
+    audiostream();
+    void load(std::array< sf::Int16,TABLE_SIZE> tableau);
+    void load(std::vector<opus_uint8> tableau);
+    void reception_donnee_audio(sf::Packet);
+    void initialization(unsigned int canaux, unsigned int taux_echantillonnage);
+    void lecture_audio();
+protected:
+
+private:
+    virtual bool onGetData(Chunk &data);
+
+    virtual void onSeek(sf::Time timeoffset);
+
+    std::queue<std::vector< sf::Int16>>m_echantillon;
+    std::size_t m_echantillon_en_cours;
+    bool m_initialiser;
+    int m_erreur;
+    OpusDecoder* m_decodeur;
+
+};
+
+#endif//SOUNDSTREAM_H
+
+
+#endif
+
 #endif//SOUNDSTREAM_H
 
 
